60e12a07ed96d43e83b2ac8bd823dd966f65c223
Eran <librealsense.eran@gmail.com>
2023-10-14 19:45:41 +0300

    remove LRS_EXTENSION_API

--- src/source.h
+++ src/source.h
@@ -13,7 +13,7 @@ namespace librealsense
     class option;
     class frame_holder;
 
-    class LRS_EXTENSION_API frame_source
+    class frame_source
     {
     public:
         frame_source(uint32_t max_publish_list_size = 16);
