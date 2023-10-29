330848723bb90698aeb5e3c868df35be794da276
Eran <librealsense.eran@gmail.com>
2023-10-29 08:08:21 +0200

    WIP add libusb-download

--- CMake/external_libusb.cmake
+++ CMake/external_libusb.cmake
@@ -5,45 +5,45 @@ include(ExternalProject)
 
 message( STATUS "Fetching libusb..." )
 
+function( get_libusb )
 
-ExternalProject_Add(
-    libusb
+    configure_file( ${CMAKE_SOURCE_DIR}/CMake/libusb-download.cmake.in
+                    ${CMAKE_BINARY_DIR}/external-projects/libusb-download/CMakeLists.txt )
+    execute_process( COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
+                     -DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}
+                     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+                     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/external-projects/libusb-download"
+                     #OUTPUT_QUIET
+                     RESULT_VARIABLE configure_ret )
+    execute_process( COMMAND "${CMAKE_COMMAND}" --build .
+                     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/external-projects/libusb-download"
+                     #OUTPUT_QUIET
+                     RESULT_VARIABLE build_ret )
 
-    # Work-around for libusb master broken on Nov 26' 2020 with introduction of v1.0.24
-    # the issue has been reported in https://github.com/libusb/libusb/issues/812
-    GIT_REPOSITORY "https://github.com/ev-mp/libusb.git"
-    GIT_TAG "2a7372db54094a406a755f0b8548b614ba8c78ec" # "v1.0.22" + Mac get_device_list hang fix
+    if( configure_ret OR build_ret )
+        message( FATAL_ERROR "Failed to download libusb" )
+    endif()
 
-    SOURCE_DIR "${CMAKE_BINARY_DIR}/third-party/libusb/"
-    BINARY_DIR "${CMAKE_BINARY_DIR}/third-party/libusb/build"
+    add_subdirectory( "${CMAKE_BINARY_DIR}/third-party/libusb/" "${CMAKE_BINARY_DIR}/third-party/libusb/build" )
 
-    UPDATE_COMMAND ${CMAKE_COMMAND} -E copy_if_different
-            ${CMAKE_SOURCE_DIR}/third-party/libusb/CMakeLists.txt
-            ${CMAKE_BINARY_DIR}/third-party/libusb/CMakeLists.txt
-    PATCH_COMMAND ""
-    TEST_COMMAND ""
+    add_library(usb INTERFACE)
+    target_include_directories( usb INTERFACE
+        $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/third-party/libusb/libusb>
+        )
+    target_link_libraries( usb INTERFACE
+        $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/third-party/libusb/build/$<CONFIG>/${CMAKE_STATIC_LIBRARY_PREFIX}usb${CMAKE_STATIC_LIBRARY_SUFFIX}>
+        )
+    set(USE_EXTERNAL_USB ON) # INTERFACE libraries can't have real deps, so targets that link with usb need to also depend on libusb
 
-    CMAKE_ARGS -DCMAKE_CXX_STANDARD_LIBRARIES=${CMAKE_CXX_STANDARD_LIBRARIES}
-            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-            -DANDROID_ABI=${ANDROID_ABI}
-            -DANDROID_STL=${ANDROID_STL}
-)
+    if (APPLE)
+      find_library(corefoundation_lib CoreFoundation)
+      find_library(iokit_lib IOKit)
+      target_link_libraries(usb INTERFACE objc ${corefoundation_lib} ${iokit_lib})
+    endif()
 
-add_library(usb INTERFACE)
-target_include_directories( usb INTERFACE
-    $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/third-party/libusb/libusb>
-    )
-target_link_libraries( usb INTERFACE
-    $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/third-party/libusb/build/$<CONFIG>/${CMAKE_STATIC_LIBRARY_PREFIX}usb${CMAKE_STATIC_LIBRARY_SUFFIX}>
-    )
-set(USE_EXTERNAL_USB ON) # INTERFACE libraries can't have real deps, so targets that link with usb need to also depend on libusb
-
-if (APPLE)
-  find_library(corefoundation_lib CoreFoundation)
-  find_library(iokit_lib IOKit)
-  target_link_libraries(usb INTERFACE objc ${corefoundation_lib} ${iokit_lib})
-endif()
+endfunction()
+get_libusb()
 
 message( STATUS "Fetching libusb - Done" )
 
